83deb9f0f6f431972a0e4ad9590b6e7f2935e68b
Xu Jie <xujie@yf-space.com>
2016-11-12 15:10:57 +0800

    init

---
+++ super-toasts/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/home/yf/workspace/eclipse/FBReaderJ-HOME/FBReaderJ/third-party/SuperToasts/bin/AndroidManifest.xml : \
+/home/yf/workspace/eclipse/FBReaderJ-HOME/FBReaderJ/third-party/SuperToasts/AndroidManifest.xml \
